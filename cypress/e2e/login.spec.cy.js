d4f007a43823d309b8e4f1b32cb14fda2bef0b78
Shirley da Silva Valtolti <shirleyvaltolti@gmail.com>
2024-08-01 21:26:48 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -12,7 +12,7 @@ describe('Orange HRM Tests', () => {
     }
 
   it('Login - Sucess', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit(/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
